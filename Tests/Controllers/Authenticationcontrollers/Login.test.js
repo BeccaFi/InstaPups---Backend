7226b64b07f483b91a554d72fc25572babf1f001
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-11 14:14:49 +0200

    Changed paths for db and server import

--- Tests/Controllers/Authenticationcontrollers/Login.test.js
+++ Tests/Controllers/Authenticationcontrollers/Login.test.js
@@ -1,9 +1,9 @@
-const { db } = require('../Database/Database');
-const {server} = require('../server/server');
+const { db } = require('../../../Database/Database');
+const { server } = require('../../../server');
 const request = require('supertest');
 
 describe('Login', () => {
-    it('a successful login should return statuscode 200 ', async () => {
+    it('a successful login should return statuscode 200', async () => {
         await db.connect();
         const res = await request(server).post('/auth/login').send({username: 'test', password: 'test'});
         expect(res.status).toBe(200);
