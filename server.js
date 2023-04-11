dba6f1b056b4ebc7df94543bf4fca0712732ebc9
Shinyn <mattias.alm@hotmail.com>
2023-04-11 16:29:56 +0200

    Added test endpoint to setup database

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const dotenv = require('dotenv').config();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser')
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const { db } = require("./Database/Database");
+const { AuthenticationRoute } = require("./Routes/Authenticationroutes");
 
-const { AuthenticationRoute } = require('./Routes/Authenticationroutes');
+db.connect();
+server.use(express.json());
+server.use("/auth", AuthenticationRoute);
 
-server.use('/auth', AuthenticationRoute);
+server.post("/test", async (req, res) => {
+  const { username, age, school } = req.body;
+  console.log(req.body);
 
+  const result = await db.Comments.insertOne({ username: username, age: age, school: school });
 
-server.listen(5050);
+  res.status(200).json(result);
+});
+
+server.listen(5051);
 
 exports.server = server;
