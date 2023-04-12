aebf3ab7cc62b2bd80b2d97c7ff2a4b7c4d8c91f
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-12 15:56:05 +0200

    Made a Databaseerror test

--- Tests/Controllers/Authenticationcontrollers/Login.test.js
+++ Tests/Controllers/Authenticationcontrollers/Login.test.js
@@ -5,21 +5,26 @@ const request = require('supertest');
 describe('Login', () => {
     it('a successful login should return statuscode 200', async () => {
         await db.connect();
-        const res = await request(server).post('/auth/login').send({username: 'test', password: 'test'});
+        const res = await request(server).post('/auth/login').send({username: 'testing', password: 'testing'});
         expect(res.status).toBe(200);
     });
 
     it('a successful login should return a token', async () => {
-        const res = await request(server).post('/auth/login').send({username: 'test', password: 'test'});
+        const res = await request(server).post('/auth/login').send({username: 'testing', password: 'testing'});
         expect(res.body.token).toBeTruthy();
     });
 
-    it('a failed login should return statuscode 401', async () => {
+    it('a failed login should return statuscode 401 if not a database-error', async () => {
         const res = await request(server).post('/auth/login').send({})
         expect(res.status).toBe(401);
     });
 
     afterAll(() => {
         db.disconnect();
+    });
+
+    it('a failed login should return statuscode 500 if a database-error', async () => {
+        const res = await request(server).post('/auth/login').send({username: 'test', password: 'test'})
+        expect(res.status).toBe(500);
     });
 });
