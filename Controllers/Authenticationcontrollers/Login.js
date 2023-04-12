618555d210604f477f1278f5dd11306d5bc93f0a
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-12 15:56:47 +0200

    Added joi-schema, call to db and adding of token

--- Controllers/Authenticationcontrollers/Login.js
+++ Controllers/Authenticationcontrollers/Login.js
@@ -1,6 +1,30 @@
 
+const joi = require('joi');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { db } = require('../../Database/Database');
 
 module.exports.Login = async (req, res) => {
 
-    
+    const schema = joi.object({
+        username: joi.string().min(3).max(36).required(),
+        password: joi.string().min(6).max(30).required()
+    });
+
+    const {error, value} = schema.validate(req.body);
+
+    if(error) {
+        return res.status(401).send(error.details[0].message);
+    }
+
+    const { username, password } = value;
+
+    const findUser = await db.Users.find({username, password}).toArray();
+
+    if (findUser.length === 1) {
+        const token = jwt.sign({username}, process.env.JWT_SECRET, {expiresIn: '1h'});
+        return res.status(200).send({token});
+    } else {
+        return res.status(500).send('Something went wrong');
+    }
 }
