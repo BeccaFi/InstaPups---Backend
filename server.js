86cfc84dcc1c1b12cdb6fe95b037679b5cafe1c9
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-12 15:55:27 +0200

    added useage of cors and cookieparser, readded db

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const dotenv = require('dotenv').config();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser')
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const { db } = require("./Database/Database");
+const { AuthenticationRoute } = require("./Routes/Authenticationroutes");
 
-const { AuthenticationRoute } = require('./Routes/Authenticationroutes');
+db.connect();
 
-server.use('/auth', AuthenticationRoute);
+server.use(cors());
+server.use(cookieParser());
+server.use(express.json());
+server.use("/auth", AuthenticationRoute);
 
-
-server.listen(5050);
+server.listen(5051);
 
 exports.server = server;
