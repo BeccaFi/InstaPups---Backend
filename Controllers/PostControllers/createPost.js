736e9721f189754514d74fc6d4c73c06938cfa47
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-14 00:14:22 +0200

    Created endpoint to create new posts

---
+++ Controllers/PostControllers/createPost.js
@@ -0,0 +1,22 @@
+const { db } = require("../../Database/Database");
+const { createPostValidation } = require("../../Validations/createPostValidation");
+
+
+
+exports.createPost = function createPost (req, res) {
+
+    const validation = createPostValidation(req.body);
+
+    if (validation.error) return res.status(400).send(validation.error.details[0].message);
+    
+    const {username} = req.user;
+    const {datePosted, text, photos} = req.body;
+
+    db.Posts.insertOne({username: username, datePosted: datePosted, content: {text: text, photos: photos}})
+    .then(result => {
+        return res.status(201).json(result);
+    })
+    .catch(err => {
+        return res.status(500).send(err);
+    })
+}
