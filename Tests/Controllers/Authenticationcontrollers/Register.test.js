2be3575c0bc130fc92dc037009913dbc7b8342bc
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-13 12:34:03 +0200

    Registration tests - 201, 400, 500

---
+++ Tests/Controllers/Authenticationcontrollers/Register.test.js
@@ -0,0 +1,31 @@
+const request = require("supertest");
+const { server } = require("../../../server");
+const { Register } = require("../../../Controllers/Authenticationcontrollers/Register");
+const { db } = require("../../../Database/Database");
+// const joi = require("joi");
+
+describe('Register new user endpoint', () => {
+
+    it('POST /register should respond with 201 if user is successfully added', async () => {
+        await db.connect();
+
+        const res = await request(server).post('/auth/register').send({username: 'testuser15', password: 'testpass', confirmPassword: 'testpass'});
+        expect(res.status).toBe(201);
+    });
+
+
+    it('POST /register should respond with 400 if body is ommitted or insufficient', async () => {
+        const res = await request(server).post('/auth/register').send({username: 'p'});
+        expect(res.status).toBe(400);
+    });
+
+    it('POST /register should notify user about duplicate entry for username', async () => {
+        const res = await request(server).post('/auth/register').send({username: 'test1', password: 'testpass', confirmPassword: 'testpass'});
+        expect(res.status).toBe(500);
+        expect(res.body).toBe('That username is already taken.');
+    });
+
+    afterAll(() => {
+        db.disconnect();
+    });
+})
