c7d5da433646dedf29577679c212adc37ba59a27
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-13 23:32:13 +0200

    Tried making a test, need help

--- Tests/Controllers/Authenticationcontrollers/Feedcontrollers/GetFeed.test.js
+++ Tests/Controllers/Authenticationcontrollers/Feedcontrollers/GetFeed.test.js
@@ -1,21 +1,25 @@
 const { db } = require('../../../../Database/Database');
+const dotenv = require('dotenv').config();
 const { server } = require('../../../../server');
 const request = require('supertest');
-const { GetFeed } = require('../../../Controllers/Feedcontrollers/GetFeed');
+const { GetFeed } = require('../../../../Controllers/Feedcontrollers/GetFeed');
+
+const jwt = require("jsonwebtoken");
 
 describe('GetFeed', () => {
+    const token = jwt.sign('testuser15', process.env.JWT_SECRET);
     it('a successful getfeed should return statuscode 200', async () => {
         await db.connect();
-        const res = await request(server).get('/feed');
+        const res = await request(server).get('/feed').set('Cookie', [`authToken=${token}`]);
         expect(res.status).toBe(200);
     });
     it('a successful getfeed should return data', async () => {
-        const res = await request(server).get('/feed');
+        const res = await request(server).get('/feed').set('Cookie', [`authToken=${token}`]);
         expect(res.body).toBeTruthy();
     });
     it('a failed getfeed should return statuscode 500', async () => {
         db.disconnect();
-        const res = await request(server).get('/feed');
+        const res = await request(server).get('/feed').set('Cookie', [`authToken=${token}`]);
         expect(res.status).toBe(500);
     }
     );
