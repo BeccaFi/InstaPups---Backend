aa85a181c647a80ddcd389fb6c7ea212a1493c62
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-13 21:58:27 +0200

    Added new feed-route and imported middleware

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const { db } = require("./Database/Database");
 const { AuthenticationRoute } = require("./Routes/Authenticationroutes");
+const { Feedroute } = require("./Routes/Feedroute");
+const { checkAuthentication } = require("./Middlewares/Authentication");
 
 db.connect();
 
@@ -14,6 +16,7 @@ server.use(cors());
 server.use(cookieParser());
 server.use(express.json());
 server.use("/auth", AuthenticationRoute);
+server.use("/feed", checkAuthentication, Feedroute)
 
 
 server.listen(5051);
