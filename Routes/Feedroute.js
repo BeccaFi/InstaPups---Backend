e24a1dedcabc207e47af91ae99e7f79ea829cb3b
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-13 23:31:51 +0200

    import of path updated

--- Routes/Feedroute.js
+++ Routes/Feedroute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const Feedroute = express.Router();
+const { authenticateUser } = require('../middleware/checkAuthentication');
 
-const {GetFeed} = require('../Controllers/Feedcontrollers/GetFeed');
+const { GetFeed } = require('../Controllers/Feedcontrollers/GetFeed');
 
-Feedroute.get('/', GetFeed);
+Feedroute.get('/', authenticateUser, GetFeed);
 
 module.exports.Feedroute = Feedroute;
