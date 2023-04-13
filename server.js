3a308de3dc91cf07e5abcded3bce55aa4ba337f6
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-13 12:29:10 +0200

    removed test endpoint

--- server.js
+++ server.js
@@ -12,14 +12,6 @@ db.connect();
 server.use(express.json());
 server.use("/auth", AuthenticationRoute);
 
-server.post("/test", async (req, res) => {
-  const { username, age, school } = req.body;
-  console.log(req.body);
-
-  const result = await db.Comments.insertOne({ username: username, age: age, school: school });
-
-  res.status(200).json(result);
-});
 
 server.listen(5051);
 
