8201583b2d4c560831c593248aa6fb7c5da7b949
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-13 12:30:17 +0200

    Added register route

--- Routes/Authenticationroutes.js
+++ Routes/Authenticationroutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const AuthenticationRoute = express.Router();
 const { Login } = require('../Controllers/Authenticationcontrollers/Login');
+const { Register } = require('../Controllers/Authenticationcontrollers/Register');
 
 AuthenticationRoute.post('/login', Login);
+AuthenticationRoute.post('/register', Register);
 
-module.exports.AuthenticationRoute  = AuthenticationRoute ;
+module.exports.AuthenticationRoute  = AuthenticationRoute;
