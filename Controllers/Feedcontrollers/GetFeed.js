e66cb4d4ab741ac4fac2e1275ec30b913d781739
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-14 10:53:53 +0200

    added joi schema and username från req query

--- Controllers/Feedcontrollers/GetFeed.js
+++ Controllers/Feedcontrollers/GetFeed.js
@@ -5,8 +5,21 @@ const jwt = require('jsonwebtoken');
 const { db } = require('../../Database/Database');
 
 module.exports.GetFeed = async (req, res) => {
+
+    const schema = joi.object({
+        username: joi.string().required(),
+    });
+
+    const { validation, error } = schema.validate(req.query);
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const { username } = validation;
+
     try {
-        const findPosts = await db.Posts.find().toArray();
+        const findPosts = await db.Posts.find({username}).toArray();
         return res.status(200).send(findPosts);
     }
     catch (error) {
