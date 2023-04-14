0108b6af8e7aa85c949f512cd56146a8387bea15
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-14 18:34:43 +0200

    Route to toggle likes on posts

--- Routes/PostRoutes.js
+++ Routes/PostRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const PostRoutes = express.Router();
 const { authenticateUser } = require('../middleware/checkAuthentication');
 const { createPost } = require('../Controllers/PostControllers/CreatePost');
+const { toggleLikePost } = require('../Controllers/PostControllers/likePost');
 
 PostRoutes.post('/create', authenticateUser, createPost);
-
+PostRoutes.patch('/like/:id', authenticateUser, toggleLikePost); // "id" = only the 24 characters within the objectid(). Ex. 64394ad1f2c2cc88bb2ce77e
 
 
 module.exports.PostRoutes = PostRoutes;
