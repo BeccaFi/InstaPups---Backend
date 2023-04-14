2a595088e5664de4671e179ada7345f362074e5e
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-14 16:38:10 +0200

    changed "validation" to "value"

--- Controllers/Feedcontrollers/GetFeed.js
+++ Controllers/Feedcontrollers/GetFeed.js
@@ -10,17 +10,21 @@ module.exports.GetFeed = async (req, res) => {
         username: joi.string().required(),
     });
 
-    const { validation, error } = schema.validate(req.query);
+    const { value, error } = schema.validate(req.query);
 
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
 
-    const { username } = validation;
+    const { username } = value;
 
     try {
         const findFollowings = await db.Users.find({username}).toArray();
+
+        if (!findFollowings[0].following) return res.status(200).send('You are not following anyone');
+
         const mappedFollowings = findFollowings[0].following.map(follower => follower);
+        console.log(mappedFollowings)
         const findPosts = await db.Posts.find({username: {$in: mappedFollowings}}).toArray();
         return res.status(200).send(findPosts);
     }
