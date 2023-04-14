00829ba8f769e61626ac2bae24812b8dc55aa896
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-14 16:39:21 +0200

    Changed username and password to match db

--- Tests/Controllers/Authenticationcontrollers/Login.test.js
+++ Tests/Controllers/Authenticationcontrollers/Login.test.js
@@ -6,13 +6,13 @@ const { Login } = require('../../../Controllers/Authenticationcontrollers/Login'
 describe('Login', () => {
     it('a successful login should return statuscode 200', async () => {
         await db.connect();
-        const res = await request(server).post('/auth/login').send({username: 'testuser15', password: 'testpass'});
+        const res = await request(server).post('/auth/login').send({username: 'Loki', password: 'hundben'});
         expect(res.status).toBe(200);
     });
 
-    it('a successful login should return a token', async () => {
-        const res = await request(server).post('/auth/login').send({username: 'testuser15', password: 'testpass'});
-        expect(res.body.token).toBeTruthy();
+    it('a successful login should return a cookie', async () => {
+        const res = await request(server).post('/auth/login').send({username: 'Loki', password: 'hundben'});
+        expect(res.headers['set-cookie']).toBeDefined();
     });
 
     it('a failed login due to joi-schema should return statuscode 400', async () => {
@@ -21,7 +21,7 @@ describe('Login', () => {
     });
 
     it('a failed login due to wrong password should return statuscode 401 if not a database-error', async () => {
-        const res = await request(server).post('/auth/login').send({username: 'testuser15', password: 'testpass1'})
+        const res = await request(server).post('/auth/login').send({username: 'Loki', password: 'hundarna'})
         expect(res.status).toBe(401);
         
     });
@@ -32,7 +32,7 @@ describe('Login', () => {
 
     it('a failed login should return statuscode 500 if a database-error', async () => {
         db.disconnect(); 
-        const res = await request(server).post('/auth/login').send({username: 'testuser15', password: 'testpass'})
+        const res = await request(server).post('/auth/login').send({username: 'Loki', password: 'hundben'})
         expect(res.status).toBe(500);
     });
 });
