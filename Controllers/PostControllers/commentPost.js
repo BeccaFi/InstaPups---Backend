46c7f3bb97dbf9d3565f824bf0f053cf50bf53d5
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-15 00:37:27 +0200

    Created controller to send comments on posts

---
+++ Controllers/PostControllers/commentPost.js
@@ -0,0 +1,22 @@
+const { ObjectId } = require("mongodb");
+const { db } = require("../../Database/Database");
+const { commentPostValidation } = require("../../Validations/commentValidation");
+
+
+exports.commentPost = async function commentPost (req, res) {
+
+    const validation = commentPostValidation(req.body);
+
+    if (validation.error) return res.status(400).send(validation.error.details[0].message);
+    
+    const {username} = req.user;
+    const {id, comment} = req.body;
+
+    db.Posts.updateOne({_id: new ObjectId(id)}, {$push: {comments: {username: username, comment: comment}}})
+    .then(result => {
+        return res.status(200).json(result);
+    })
+    .catch(err => {
+        return res.status(500).send(err);
+    })
+}
