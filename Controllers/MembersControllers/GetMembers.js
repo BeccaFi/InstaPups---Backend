e4a2b33d414621530e3926a3713e5acd32a4bc5c
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-16 16:41:40 +0200

    Fetches data from db and sents it back

---
+++ Controllers/MembersControllers/GetMembers.js
@@ -0,0 +1,16 @@
+
+const joi = require('joi');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { db } = require('../../Database/Database');
+
+module.exports.GetMembers = async (req, res) => {
+    try {
+        const findMembers = await db.Users.find().toArray();
+        return res.status(200).json(findMembers);
+    }
+    catch (error) {
+        return res.status(500).json('Something went wrong');
+    }
+
+}
