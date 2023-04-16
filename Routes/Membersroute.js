5288571fdbb5e9e08577940ca703aaafe80b3f72
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-16 16:57:04 +0200

    Added /follows endpoint to Route

--- Routes/Membersroute.js
+++ Routes/Membersroute.js
@@ -2,7 +2,9 @@ const express = require('express');
 const Membersroute = express.Router();
 const { authenticateUser } = require('../middleware/checkAuthentication');
 const { GetMembers } = require('../Controllers/MembersControllers/GetMembers');
+const { GetFollows } = require('../Controllers/MembersControllers/GetFollows');
 
 Membersroute.get('/', authenticateUser, GetMembers);
+Membersroute.get('/follows', authenticateUser, GetFollows);
 
 module.exports.Membersroute = Membersroute;
