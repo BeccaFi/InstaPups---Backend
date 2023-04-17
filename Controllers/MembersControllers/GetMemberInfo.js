388f1728b09e6e6c5e61235d531c48484830a4f3
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-17 20:25:55 +0200

    Added new controller, sends member info to Post.js

---
+++ Controllers/MembersControllers/GetMemberInfo.js
@@ -0,0 +1,30 @@
+const { db } = require("../../Database/Database");
+const joi = require("joi");
+
+exports.GetMemberInfo = async function GetMemberInfo (req, res) {
+    
+    const schema = joi.object({
+        username: joi.string().required()
+    });
+
+    const {value, error } = schema.validate(req.query);
+
+    const {username} = value;
+
+    if (error) {
+        return res.status(400).json('Invalid username');
+    }
+
+    try {
+    const Userinfo = await db.Users.find({username}).toArray();
+
+    res.status(200).json(Userinfo[0]);
+    }
+
+    catch (error) {
+        return res.status(500).json('Something went wrong with the database');
+    }
+}
+
+
+    
