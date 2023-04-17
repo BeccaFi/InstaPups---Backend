527aa7a03741181b3abe7de4f07e1a92f0c3f2f2
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-17 15:46:00 +0200

    Deleted unfollow controller/endpoint

--- Routes/Membersroute.js
+++ Routes/Membersroute.js
@@ -5,12 +5,10 @@ const { GetMembers } = require('../Controllers/MembersControllers/GetMembers');
 const { GetFollows } = require('../Controllers/MembersControllers/GetFollows');
 const { GetMember } = require('../Controllers/MembersControllers/GetMember');
 const { FollowMember } = require('../Controllers/MembersControllers/FollowMember');
-const { UnFollowMember } = require('../Controllers/MembersControllers/UnFollowMember');
 
 Membersroute.get('/', authenticateUser, GetMembers);
 Membersroute.get('/follows', authenticateUser, GetFollows);
 Membersroute.get('/:id', authenticateUser, GetMember);
-Membersroute.post('/follow', authenticateUser, FollowMember)
-Membersroute.delete('/unfollow', authenticateUser, UnFollowMember)
+Membersroute.patch('/follow', authenticateUser, FollowMember);
 
 module.exports.Membersroute = Membersroute;
