d86cd6280f7dfd37db87d226f1802e66a5ea3e15
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-17 20:25:14 +0200

    added new controller endpoint

--- Routes/Membersroute.js
+++ Routes/Membersroute.js
@@ -5,10 +5,12 @@ const { GetMembers } = require('../Controllers/MembersControllers/GetMembers');
 const { GetFollows } = require('../Controllers/MembersControllers/GetFollows');
 const { GetMember } = require('../Controllers/MembersControllers/GetMember');
 const { FollowMember } = require('../Controllers/MembersControllers/FollowMember');
+const { GetMemberInfo } = require('../Controllers/MembersControllers/GetMemberInfo');
 
 Membersroute.get('/', authenticateUser, GetMembers);
 Membersroute.get('/follows', authenticateUser, GetFollows);
 Membersroute.get('/:id', authenticateUser, GetMember);
 Membersroute.patch('/follow', authenticateUser, FollowMember);
+Membersroute.get('/memberinfo', authenticateUser, GetMemberInfo);
 
 module.exports.Membersroute = Membersroute;
