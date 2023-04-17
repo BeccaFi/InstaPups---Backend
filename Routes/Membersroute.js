c6676776d685259e72e39822ee82fd5efa543ea5
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-17 13:58:19 +0200

    Added controller to Route

--- Routes/Membersroute.js
+++ Routes/Membersroute.js
@@ -4,9 +4,11 @@ const { authenticateUser } = require('../middleware/checkAuthentication');
 const { GetMembers } = require('../Controllers/MembersControllers/GetMembers');
 const { GetFollows } = require('../Controllers/MembersControllers/GetFollows');
 const { GetMember } = require('../Controllers/MembersControllers/GetMember');
+const { FollowMember } = require('../Controllers/MembersControllers/FollowMember');
 
 Membersroute.get('/', authenticateUser, GetMembers);
 Membersroute.get('/follows', authenticateUser, GetFollows);
 Membersroute.get('/:id', authenticateUser, GetMember);
+Membersroute.post('/follow', authenticateUser, FollowMember)
 
 module.exports.Membersroute = Membersroute;
