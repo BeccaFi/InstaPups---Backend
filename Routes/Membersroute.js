2154ce42e68cca3129965ce799b87e15170cf7c7
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-17 20:57:51 +0200

    Moved memberinfo controller to top to work

--- Routes/Membersroute.js
+++ Routes/Membersroute.js
@@ -7,10 +7,11 @@ const { GetMember } = require('../Controllers/MembersControllers/GetMember');
 const { FollowMember } = require('../Controllers/MembersControllers/FollowMember');
 const { GetMemberInfo } = require('../Controllers/MembersControllers/GetMemberInfo');
 
+Membersroute.get('/memberinfo', authenticateUser, GetMemberInfo);
 Membersroute.get('/', authenticateUser, GetMembers);
 Membersroute.get('/follows', authenticateUser, GetFollows);
 Membersroute.get('/:id', authenticateUser, GetMember);
 Membersroute.patch('/follow', authenticateUser, FollowMember);
-Membersroute.get('/memberinfo', authenticateUser, GetMemberInfo);
+
 
 module.exports.Membersroute = Membersroute;
