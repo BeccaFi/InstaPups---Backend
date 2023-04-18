898dc626d7469a252c91004a682c6d1195c8b60b
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-18 13:50:40 +0200

    Added 404 code if username not found

--- Controllers/MembersControllers/FollowMember.js
+++ Controllers/MembersControllers/FollowMember.js
@@ -22,6 +22,9 @@ module.exports.FollowMember = async (req, res) => {
     try {
 
         const findUser = await db.Users.find({username}).toArray();
+
+        if (findUser.length === 0) return res.status(404).json('User not found');
+        
         if (findUser[0].following.includes(followMember)) {
             const unfollowings = db.Users.updateOne({username}, {$pull: {following: followMember}})
             return res.status(200).json("unfollowed");
