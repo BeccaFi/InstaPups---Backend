b34a655b1da92b9b2d0691adc564d251aba5de97
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-18 19:45:39 +0200

    Sends back the new comment, so frontend canrender

--- Controllers/PostControllers/commentPost.js
+++ Controllers/PostControllers/commentPost.js
@@ -3,20 +3,28 @@ const { db } = require("../../Database/Database");
 const { commentPostValidation } = require("../../Validations/commentValidation");
 
 
-exports.commentPost = async function commentPost (req, res) {
-
+exports.commentPost = async function commentPost(req, res) {
     const validation = commentPostValidation(req.body);
-
+  
     if (validation.error) return res.status(400).json(validation.error.details[0].message);
-    
-    const {username} = req.user;
-    const {id, comment} = req.body;
-
-    db.Posts.updateOne({_id: new ObjectId(id)}, {$push: {comments: {username: username, comment: comment}}})
-    .then(result => {
-        return res.status(200).json(result);
-    })
-    .catch(err => {
+  
+    const { username } = req.user;
+    const { id, comment } = req.body;
+  
+    db.Posts.updateOne(
+      { _id: new ObjectId(id) },
+      {
+        $push: {
+          comments: { username: username, comment: comment },
+        },
+      }
+    )
+      .then(async (result) => {
+        const post = await db.Posts.findOne({ _id: new ObjectId(id) });
+        const newComment = post.comments[post.comments.length - 1];
+        return res.status(200).json({ ...result, newComment });
+      })
+      .catch((err) => {
         return res.status(500).send(err);
-    })
-}
+      });
+  };
