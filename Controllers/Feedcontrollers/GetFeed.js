701d5b5faf215a85f409f7fdde7d122dee47235b
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-18 13:49:56 +0200

    Added 404 code if user not found

--- Controllers/Feedcontrollers/GetFeed.js
+++ Controllers/Feedcontrollers/GetFeed.js
@@ -9,6 +9,8 @@ module.exports.GetFeed = async (req, res) => {
     try {
         const findFollowings = await db.Users.find({username}).toArray();
 
+        if (findFollowings.length === 0) return res.status(404).json('User not found');
+
         if (!findFollowings[0].following) return res.status(200).json('You are not following anyone');
 
         const mappedFollowings = findFollowings[0].following.map(follower => follower);
