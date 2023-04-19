687f740d8663f3a8454a3ca3d9c761fec5167b6a
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-19 09:50:23 +0200

    Renamed to GetUserInfo

--- Routes/Membersroute.js
+++ Routes/Membersroute.js
@@ -5,9 +5,9 @@ const { GetMembers } = require('../Controllers/MembersControllers/GetMembers');
 const { GetFollows } = require('../Controllers/MembersControllers/GetFollows');
 const { GetMember } = require('../Controllers/MembersControllers/GetMember');
 const { FollowMember } = require('../Controllers/MembersControllers/FollowMember');
-const { GetMemberInfo } = require('../Controllers/MembersControllers/GetMemberInfo');
+const { GetUserInfo } = require('../Controllers/MembersControllers/GetUserInfo');
 
-Membersroute.get('/memberinfo', authenticateUser, GetMemberInfo);
+Membersroute.get('/userinfo', authenticateUser, GetUserInfo);
 Membersroute.get('/', authenticateUser, GetMembers);
 Membersroute.get('/follows', authenticateUser, GetFollows);
 Membersroute.get('/:id', authenticateUser, GetMember);
