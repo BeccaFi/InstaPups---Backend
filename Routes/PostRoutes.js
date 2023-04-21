b1ee08659206cdd1021c2b26c01df2d20b5b441f
Shinyn <mattias.alm@hotmail.com>
2023-04-21 16:29:02 +0200

    Added PATCH route to posts

--- Routes/PostRoutes.js
+++ Routes/PostRoutes.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const PostRoutes = express.Router();
-const { authenticateUser } = require('../middleware/checkAuthentication');
-const { createPost } = require('../Controllers/PostControllers/CreatePost');
-const { toggleLikePost } = require('../Controllers/PostControllers/likePost');
-const { commentPost } = require('../Controllers/PostControllers/commentPost');
+const { authenticateUser } = require("../middleware/checkAuthentication");
+const { createPost } = require("../Controllers/PostControllers/CreatePost");
+const { toggleLikePost } = require("../Controllers/PostControllers/likePost");
+const { commentPost } = require("../Controllers/PostControllers/commentPost");
+const { updatePost } = require("../Controllers/PostControllers/updatePost");
 
-PostRoutes.post('/create', authenticateUser, createPost);
+PostRoutes.post("/create", authenticateUser, createPost);
 
-PostRoutes.patch('/:id/like', authenticateUser, toggleLikePost); // "id" = only the 24 characters within the objectid(). Ex. 64394ad1f2c2cc88bb2ce77e
-PostRoutes.patch('/:id/comment', authenticateUser, commentPost); //should be patch for updates BUT we can't send long comments with whitespaces etc. in params/queries
-
+PostRoutes.patch("/:id/like", authenticateUser, toggleLikePost); // "id" = only the 24 characters within the objectid(). Ex. 64394ad1f2c2cc88bb2ce77e
+PostRoutes.patch("/:id/comment", authenticateUser, commentPost); //should be patch for updates BUT we can't send long comments with whitespaces etc. in params/queries
+PostRoutes.patch("/update/:id", authenticateUser, updatePost);
 
 module.exports.PostRoutes = PostRoutes;
