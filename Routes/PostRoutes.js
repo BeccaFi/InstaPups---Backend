f73b69f9311d3bc30d26d098b74e03a938c87f05
Shinyn <mattias.alm@hotmail.com>
2023-04-21 18:58:20 +0200

    Added deleteRoute to PostRoutes

--- Routes/PostRoutes.js
+++ Routes/PostRoutes.js
@@ -5,11 +5,13 @@ const { createPost } = require("../Controllers/PostControllers/CreatePost");
 const { toggleLikePost } = require("../Controllers/PostControllers/likePost");
 const { commentPost } = require("../Controllers/PostControllers/commentPost");
 const { updatePost } = require("../Controllers/PostControllers/updatePost");
+const { deletePost } = require("../Controllers/PostControllers/deletePost");
 
 PostRoutes.post("/create", authenticateUser, createPost);
 
 PostRoutes.patch("/:id/like", authenticateUser, toggleLikePost); // "id" = only the 24 characters within the objectid(). Ex. 64394ad1f2c2cc88bb2ce77e
 PostRoutes.patch("/:id/comment", authenticateUser, commentPost); //should be patch for updates BUT we can't send long comments with whitespaces etc. in params/queries
 PostRoutes.patch("/update/:id", authenticateUser, updatePost);
+PostRoutes.delete("/:id", authenticateUser, deletePost);
 
 module.exports.PostRoutes = PostRoutes;
