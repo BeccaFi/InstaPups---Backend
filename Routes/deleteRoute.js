f3f8fdce4035590a4a5214af2a48c7e86c8650a0
Shinyn <mattias.alm@hotmail.com>
2023-04-21 18:57:27 +0200

    Removed deleteRoute

--- Routes/deleteRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const deleteRoute = express.Router();
-const { authenticateUser } = require("../middleware/checkAuthentication");
-const { deletePost } = require("../Controllers/Deletecontroller/deletePost");
-
-deleteRoute.delete("/:id", authenticateUser, deletePost);
-
-module.exports.deleteRoute = deleteRoute;
