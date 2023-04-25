d61fe6eecd9cdd33736c4aee9918a0e72478c50b
Shinyn <mattias.alm@hotmail.com>
2023-04-25 21:01:49 +0200

    Added validation to deletePost

--- Controllers/PostControllers/deletePost.js
+++ Controllers/PostControllers/deletePost.js
@@ -1,14 +1,18 @@
 const { db } = require("../../Database/Database");
 const { ObjectId } = require("mongodb");
+const { verify } = require("jsonwebtoken");
+const { deletePostValidation } = require("../../Validations/deletePostValidation");
 
 exports.deletePost = async function deletePost(req, res) {
   const { id } = req.params;
   const { username } = req.user;
 
+  const validation = deletePostValidation(id);
+  if (validation.error) return res.status(400).json(validation.error.details[0].message);
+
   try {
     const findPost = await db.Posts.findOne({ _id: new ObjectId(id) });
-    if (findPost.username !== username)
-      return res.status(401).json("You are not authorized to delete this post");
+    if (findPost.username !== username) return res.status(401).json("You are not authorized to delete this post");
 
     const deletePost = await db.Posts.deleteOne({ _id: new ObjectId(id) });
     return res.status(200).json(deletePost);
