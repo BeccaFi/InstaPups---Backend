3f88b68e60206019b34820afb322273034041b72
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-25 13:40:51 +0200

    Added logged in user username to response

--- Controllers/MembersControllers/GetUserInfo.js
+++ Controllers/MembersControllers/GetUserInfo.js
@@ -7,18 +7,19 @@ module.exports.GetUserInfo = async (req, res) => {
 
     const {value, error } = getUserInfoValidation(req.query);
 
-    const {username} = value;
+    const {postUsername} = value;
+    const {username} = req.user;
 
     if (error) {
         return res.status(400).json('Invalid username');
     }
 
     try {
-    const Userinfo = await db.Users.find({username}).toArray();
+    const Userinfo = await db.Users.find({username: postUsername}).toArray();
 
     if (Userinfo.length === 0) return res.status(404).json('User not found');
 
-    res.status(200).json(Userinfo[0]);
+    return res.status(200).json({loggedInUser: username, postUser: Userinfo[0]});
     }
 
     catch (error) {
