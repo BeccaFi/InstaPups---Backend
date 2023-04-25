1cb982c62f44601016ecb45082c1da3a2d515ba9
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-25 18:21:50 +0200

    Added more response content to all outcomes

--- Controllers/MembersControllers/GetMember.js
+++ Controllers/MembersControllers/GetMember.js
@@ -1,24 +1,42 @@
 const { ObjectId } = require('mongodb');
 const { db } = require('../../Database/Database');
+const { userProfileValidation } = require('../../Validations/userProfileValidation');
 
 module.exports.GetMember = async (req, res) => {
-    const id = req.params.id.substring(1);
-    
+
+    const validation = userProfileValidation(req.params);
+    if (validation.error) return res.status(400).json(validation.error.details[0].message);
+
+    const {username} = req.user;
+    const {id} = req.params;
+
+    const loggedInUser = await db.Users.findOne({username: username}, {projection: {password: 0}});
+    const follows = loggedInUser.following;
+    const findUser = await db.Users.findOne({_id: new ObjectId(id)}, {projection: {password: 0}});
+    const findUserUsername = findUser.username; //To get the username of whoevers' profile we're viewing, not our own username from req.user.
+
 
     try {
-        const findUserInformation = await db.Users.find({_id: new ObjectId(id)}).toArray();
 
-        if (findUserInformation.length === 0) {
-            return res.status(404).json('User not found');
-        }
-        
-        const findUsersPosts = await db.Posts.find({username: findUserInformation[0].username}).toArray();
+        if (findUser.length === 0) return res.status(404).json('User not found');
 
-        if (findUsersPosts.length === 0) {
-            return res.status(404).json('User has no posts');
+        if(id === loggedInUser._id.toString()) {
+            const userPosts = await db.Posts.find({username: username}).sort({datePosted: -1}).toArray();
+            return res.status(200).json({user: loggedInUser, loggedInUser: loggedInUser, posts: userPosts});
         }
 
-        return res.status(200).json({user: findUserInformation[0], posts: findUsersPosts});
+        if(follows.includes(findUserUsername)) {
+            const userPosts = await db.Posts.find({username: findUserUsername}).sort({datePosted: -1}).toArray();
+            if (userPosts.length === 0) return res.status(200).json({user: findUser, loggedInUser: loggedInUser, posts: userPosts, postMessage: `${findUserUsername} has no posts`});
+            return res.status(200).json({user: findUser, loggedInUser: loggedInUser, posts: userPosts});
+        }
+
+        const userPosts = await db.Posts.find({username: findUserUsername}).sort({datePosted: -1}).toArray();
+        res.status(200).json({user: findUser, loggedInUser: loggedInUser, posts: userPosts, followMessage: `You need to follow ${findUserUsername} to see their posts.`});
+        return;
+        // const findUsersPosts = await db.Posts.find({username: findUserInformation.username}).toArray();
+
+        // return res.status(200).json({user: findUserInformation[0], posts: findUsersPosts});
     } catch (error) {
         return res.status(500).json('Something went wrong');
     }
