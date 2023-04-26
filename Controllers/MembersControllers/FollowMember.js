441060aa261909aa228d692076941101e416bc13
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-27 01:03:42 +0200

    Shortened syntax in if statements a little bit

--- Controllers/MembersControllers/FollowMember.js
+++ Controllers/MembersControllers/FollowMember.js
@@ -3,19 +3,14 @@ const { followMemberValidation } = require('../../Validations/followMemberValida
 
 module.exports.FollowMember = async (req, res) => {
 
-    
-
     const { value, error } = followMemberValidation(req.body);
 
     const followMember = value.username;
     const { username } = req.user;
     
-    if (error) {
-        return res.status(400).json(error.details[0].message);
-    }
-    if (username === followMember) {
-        return res.status(400).json('You cannot follow yourself');
-    }
+    if (error) return res.status(400).json(error.details[0].message);
+    
+    if (username === followMember) return res.status(400).json('You cannot follow yourself');
 
     try {
 
@@ -24,12 +19,11 @@ module.exports.FollowMember = async (req, res) => {
         if (findUser.length === 0) return res.status(404).json('User not found');
         
         if (findUser[0].following.includes(followMember)) {
-            const unfollowings = db.Users.updateOne({username}, {$pull: {following: followMember}})
+            const unfollowings = await db.Users.updateOne({username}, {$pull: {following: followMember}});
             return res.status(200).json("unfollowed");
         }
-        const followings = db.Users.updateOne({username}, {$push: {following: followMember}})
-        
-        return res.status(200).json("followed");
+        const followings = db.Users.updateOne({username}, {$push: {following: followMember}});
+        return res.status(200).json("Followed");
     }
 
     catch (error) {
