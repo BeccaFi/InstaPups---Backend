5f2c616fe8352a3ba9f2b2a5823f996f305f7253
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-27 01:23:04 +0200

    Changed folder name to Middleswares (big M + 's')

---
+++ Middlewares/checkAuthentication.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
+
+const authenticateUser = (req, res, next) => {
+  const authToken = req.cookies.authToken;
+
+  if (!authToken) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(authToken, process.env.JWT_SECRET);
+    req.user = decodedToken;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+module.exports = { authenticateUser };
