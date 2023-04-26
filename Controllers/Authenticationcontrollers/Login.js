81cf2f21ad2e1711a6f719dd90b19f3c1b18f5dc
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-27 00:56:48 +0200

    Moved if error to one row only

--- Controllers/Authenticationcontrollers/Login.js
+++ Controllers/Authenticationcontrollers/Login.js
@@ -8,11 +8,10 @@ module.exports.Login = async (req, res) => {
 
     const {error, value} = loginValidation(req.body);
 
-    if(error) {
-        return res.status(400).send(error.details[0].message);
-    }
+    if(error) return res.status(400).send(error.details[0].message);
 
     const { username, password } = value;
+    
     try {
     const findUser = await db.Users.find({username}).toArray();
 
