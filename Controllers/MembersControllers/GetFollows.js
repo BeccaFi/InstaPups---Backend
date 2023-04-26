9025524c6e8973aadb2ada20567047467deda007
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-27 01:05:48 +0200

    Shortened syntax for if() and db find

--- Controllers/MembersControllers/GetFollows.js
+++ Controllers/MembersControllers/GetFollows.js
@@ -5,14 +5,14 @@ module.exports.GetFollows = async (req, res) => {
     const { username } = req.user;
 
     try {
-        const findFollowings = await db.Users.find({username}).toArray();
+        const findFollowings = await db.Users.findOne({username});
 
-        if (findFollowings.length === 0) return res.status(404).json('User not found');
+        if (!findFollowings) return res.status(404).json('User not found');
         
-        return res.status(200).json(findFollowings[0].following);
+        return res.status(200).json(findFollowings.following);
     }
     catch (error) {
         return res.status(500).json('Something went wrong');
-    }
+    };
 
 }
