07ad9cc7db89b27fc78756d0a90dcda3a67b9db0
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-27 17:27:49 +0200

    Removed some spacing

--- Controllers/MembersControllers/GetMembers.js
+++ Controllers/MembersControllers/GetMembers.js
@@ -5,8 +5,6 @@ module.exports.GetMembers = async (req, res) => {
     try {
         const findMembers = await db.Users.find().toArray();
 
-        
-
         if (findMembers.length === 0) return res.status(404).json('No members found');
         const membersWithoutYou = findMembers.filter(member => member.username !== req.user.username);
         return res.status(200).json(membersWithoutYou);
